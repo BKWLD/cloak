696d0fb25643774ff702d66ba13de62c32ab2fed
Robert Reinhard <robert.reinhard@bukwild.com>
2021-04-09 08:53:36 -0700

    Another fix for removeListener

--- components/responsive-craft-visual.vue
+++ components/responsive-craft-visual.vue
@@ -33,7 +33,7 @@ export default
 		@mounted = true
 
 	destroyed: ->
-		@isLanscapeMediaQuery.removeListener 'resize', @checkIsLandscape
+		@isLanscapeMediaQuery?.removeListener @checkIsLandscape
 
 	computed:
 
