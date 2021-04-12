7491096f202332f2c69669d77b2d91729c613523
Robert Reinhard <robert.reinhard@bukwild.com>
2021-04-11 17:52:15 -0700

    Add a higher level to cloak components

--- modules/component-registration.js
+++ modules/component-registration.js
@@ -9,6 +9,11 @@ export default function () {
 
 			// Make these root level components
 			pathPrefix: false,
+
+			// Load at a higher level so project components can override them. I
+			// chose "2" thinking that an explicit component library loaded from the
+			// project may use "1".
+			level: 2,
 		})
 	})
 }
