4ba1a73d3a644b31e050b4f8518741f7ac6cdf2f
Robert Reinhard <robert.reinhard@bukwild.com>
2021-06-23 16:56:48 -0700

    Move shared responsive-visual code into a mixin

--- components/responsive-contentful-visual.vue
+++ components/responsive-contentful-visual.vue
@@ -1,97 +1,15 @@
 <!-- Conditionally render landscape or portrait visual instances -->
 
 <script lang='coffee'>
+import responsiveVisualMixin from '../mixins/visual/responsive-visual'
 import ContentfulVisual, { makeSrcset } from './contentful-visual'
 import { ucFirst } from '../services/helpers'
 export default
 	name: 'ResponsiveContentfulVisual'
 
-	props: {
-
-		# Merge craft-visual props
-		...ContentfulVisual.props
-
-		# Make explicit landscape and portrait props
-		landscapeImage: Object|Array
-		portraitImage: Object|Array
-		landscapeVideo: Object|Array
-		portraitVideo: Object|Array
-	}
-
-	data: ->
-		mounted: false
-		isLandscape: null
-
-	mounted: ->
-		@isLanscapeMediaQuery = window.matchMedia '(orientation: landscape)'
-		@checkIsLandscape @isLanscapeMediaQuery
-		@isLanscapeMediaQuery.addListener @checkIsLandscape
-		@mounted = true
-
-	destroyed: ->
-		@isLanscapeMediaQuery?.removeListener @checkIsLandscape
-
-	computed:
-
-		# Visual configs
-		landscape: -> @makeConfig @getAsset('image', 'landscape'),
-			@getAsset('video', 'landscape')
-
-		portrait: -> @makeConfig @getAsset('image', 'portrait'),
-			@getAsset('video', 'portrait')
-
-		# Do we have unique landscape and portrait configs?
-		isResponsive: -> !!(@landscape and @portrait)
-
-		# The config used when there is both landscape and portrait assets. The video
-		# prop will only be set once the viewport can be measured.
-		responsiveConfig: ->
-			return unless @isResponsive
-			{
-				...@landscape
-				props: {
-					...@landscape.props
-					video: @responsiveVideo
-				}
-				class: 'responsive-visual'
-				style:
-					'--landscape-aspect': @makeAspectStyle 'landscape'
-					'--portrait-aspect': @makeAspectStyle 'portrait'
-			}
-
-		# If there are both landscape and portrait videos, wait till the page is
-		# mounted to decide which to show.  This prevents issues with differences
-		# between SSR and client. Otherwise, just use whichever we have.
-		responsiveVideo: ->
-			landscape = @landscape.props.video
-			portrait = @portrait.props.video
-			if landscape and portrait
-				return unless @mounted
-				if @isLandscape then landscape else portrait
-			else landscape || portrait
-
-		# Make responsive sources
-		responsiveSources: ->
-			return unless @isResponsive
-			[
-				{
-					attrs:
-						media: '(orientation: landscape)'
-						srcset: makeSrcset @landscape.props.image
-						sizes: @sizes
-				}
-				{
-					attrs:
-						media: '(orientation: portrait)'
-						srcset: makeSrcset @portrait.props.image
-						sizes: @sizes
-				}
-			]
+	mixins: [ responsiveVisualMixin ]
 
 	methods:
-
-		# Store whether orientation is currently landscape
-		checkIsLandscape: (e) -> @isLandscape = e.matches
 
 		# Get the specified asset
 		# mediaType: image|video
@@ -99,26 +17,8 @@ export default
 		getAsset: (mediaType, viewportType) ->
 			@[viewportType + ucFirst(mediaType)]
 
-		# Make the aspect css style
-		makeAspectStyle: (viewportType) ->
-			return unless image = @[viewportType].props.image
-			aspect = image.width / image.height
-			return "#{1 / aspect * 100}%"
-
-		# Make the config object for the create function by keeping all data and
-		# props except for replacing landscape and portrait with the asset itself
-		makeConfig: (image, video) ->
-			return unless image or video
-			on: loaded: => @$emit 'loaded'
-			props: {
-				...@$props
-				image
-				video
-				landscapeImage: undefined
-				portraitImage: undefined
-				landscapeVideo: undefined
-				portraitVideo: undefined
-			}
+		# Passthru the logic that makes a srceset from the passed in prop
+		makeSrcset: makeSrcset
 
 	# Make the appropriate visual instance
 	render: (create) ->
@@ -139,17 +39,5 @@ export default
 			image: undefined
 			video: undefined
 		}}, @$slots.default
+
 </script>
-
-<!-- ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– -->
-
-<style lang='stylus' scoped>
-
-// Add responsive aspect ratio
-.responsive-visual >>> .vv-aspect-shim
-	@media(orientation landscape)
-		padding-top var(--landscape-aspect) !important
-	@media(orientation portrait)
-		padding-top var(--portrait-aspect) !important
-
-</style>
--- components/responsive-craft-visual.vue
+++ components/responsive-craft-visual.vue
@@ -1,100 +1,29 @@
 <!-- Conditionally render landscape or portrait visual instances -->
 
 <script lang='coffee'>
+import responsiveVisualMixin from '../mixins/visual/responsive-visual'
 import CraftVisual, { getAssetObject, makeSrcset } from './craft-visual'
 import { ucFirst } from '../services/helpers'
 export default
 	name: 'ResponsiveCraftVisual'
 
+	mixins: [ responsiveVisualMixin ]
+
 	props: {
+		...responsiveVisualMixin.props
 
-		# Merge craft-visual props
-		...CraftVisual.props
-
-		# Make explicit landscape and portrait props
-		landscapeImage: Object|Array
-		portraitImage: Object|Array
-		landscapeVideo: Object|Array
-		portraitVideo: Object|Array
+		# Add support for array props
+		landscapeImage: Object | Array
+		portraitImage: Object | Array
+		landscapeVideo: Object | Array
+		portraitVideo: Object | Array
 
 		# Shorthands, for passing a supertable with landscape or portrait object
-		image: Object|Array
-		video: Object|Array
+		image: Object | Array
+		video: Object | Array
 	}
 
-	data: ->
-		mounted: false
-		isLandscape: null
-
-	mounted: ->
-		@isLanscapeMediaQuery = window.matchMedia '(orientation: landscape)'
-		@checkIsLandscape @isLanscapeMediaQuery
-		@isLanscapeMediaQuery.addListener @checkIsLandscape
-		@mounted = true
-
-	destroyed: ->
-		@isLanscapeMediaQuery?.removeListener @checkIsLandscape
-
-	computed:
-
-		# Visual configs
-		landscape: -> @makeConfig @getAsset('image', 'landscape'),
-			@getAsset('video', 'landscape')
-		portrait: -> @makeConfig @getAsset('image', 'portrait'),
-			@getAsset('video', 'portrait')
-
-		# Do we have unique landscape and portrait configs?
-		isResponsive: -> !!(@landscape and @portrait)
-
-		# The config used when there is both landscape and portrait assets. The video
-		# prop will only be set once the viewport can be measured.
-		responsiveConfig: ->
-			return unless @isResponsive
-			{
-				...@landscape
-				props: {
-					...@landscape.props
-					video: @responsiveVideo
-				}
-				class: 'responsive-visual'
-				style:
-					'--landscape-aspect': @makeAspectStyle 'landscape'
-					'--portrait-aspect': @makeAspectStyle 'portrait'
-			}
-
-		# If there are both landscape and portrait videos, wait till the page is
-		# mounted to decide which to show.  This prevents issues with differences
-		# between SSR and client. Otherwise, just use whichever we have.
-		responsiveVideo: ->
-			landscape = @landscape.props.video
-			portrait = @portrait.props.video
-			if landscape and portrait
-				return unless @mounted
-				if @isLandscape then landscape else portrait
-			else landscape || portrait
-
-		# Make responsive sources
-		responsiveSources: ->
-			return unless @isResponsive
-			[
-				{
-					attrs:
-						media: '(orientation: landscape)'
-						srcset: makeSrcset @landscape.props.image
-						sizes: @sizes
-				}
-				{
-					attrs:
-						media: '(orientation: portrait)'
-						srcset: makeSrcset @portrait.props.image
-						sizes: @sizes
-				}
-			]
-
 	methods:
-
-		# Store whether orientation is currently landscape
-		checkIsLandscape: (e) -> @isLandscape = e.matches
 
 		# Get the specified asset
 		# mediaType: image|video
@@ -107,27 +36,6 @@ export default
 
 			# Get explicit value
 			else getAssetObject @[viewportType + ucFirst(mediaType)]
-
-		# Make the aspect css style
-		makeAspectStyle: (viewportType) ->
-			return unless image = @[viewportType].props.image
-			aspect = image.width / image.height
-			return "#{1 / aspect * 100}%"
-
-		# Make the config object for the create function by keeping all data and
-		# props except for replacing landscape and portrait with the asset itself
-		makeConfig: (image, video) ->
-			return unless image or video
-			on: loaded: => @$emit 'loaded'
-			props: {
-				...@$props
-				image
-				video
-				landscapeImage: undefined
-				portraitImage: undefined
-				landscapeVideo: undefined
-				portraitVideo: undefined
-			}
 
 	# Make the appropriate visual instance
 	render: (create) ->
@@ -150,16 +58,3 @@ export default
 		}}, @$slots.default
 
 </script>
-
-<!-- ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– -->
-
-<style lang='stylus' scoped>
-
-// Add responsive aspect ratio
-.responsive-visual >>> .vv-aspect-shim
-	@media(orientation landscape)
-		padding-top var(--landscape-aspect) !important
-	@media(orientation portrait)
-		padding-top var(--portrait-aspect) !important
-
-</style>
